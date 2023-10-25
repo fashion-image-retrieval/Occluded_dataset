8fffd7020236b382078d8e1e709d7566e0ec73d4
Zhiwen Yan <48267656+Yanzhiwen001@users.noreply.github.com>
2023-10-25 15:27:43 -0400

    Delete old occlusion_dataloader directory

--- occlusion_dataloader/dataset/__init__.py
+++
@@ -1,9 +0,0 @@
-
-from .import utils
-from .base import BaseDataset
-
-
-
-def load(name, root, mode, transform = None):
-    return _type[name](root = root, mode = mode, transform = transform)
-    
