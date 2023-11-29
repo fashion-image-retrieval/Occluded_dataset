6a71c4a34e775aa6b74e8d4fa73b95dcc670f63f
Jimin Sohn <sjm7396@naver.com>
2023-11-29 14:07:27 -0500

    Update make_inshop_object_hand_ratio.py

--- make_inshop_object_hand_ratio.py
+++ make_inshop_object_hand_ratio.py
@@ -26,24 +26,6 @@ mask_save_path = '../dataset/Inshop_Ratio5_Bottom_Objectoccluded/inshop_ratio5_b
 if not os.path.exists(mask_save_path):
     os.makedirs(mask_save_path)
 
-##### Inshop + Hand occlusion #####
-# save_path = '../dataset/Inshop_Small_Handoccluded/'
-# if not os.path.exists(save_path):
-#     os.makedirs(save_path)
-#
-# img_save_path = '../dataset/Inshop_Small_Handoccluded/small_hand_occluded_img/'
-# if not os.path.exists(img_save_path):
-#     os.makedirs(img_save_path)
-#
-# mask_save_path = '../dataset/Inshop_Small_Handoccluded/small_hand_occluded_mask/'
-# if not os.path.exists(mask_save_path):
-#     os.makedirs(mask_save_path)
-#
-# data_root = '../dataset/256_inshop_img/'
-# mask_path = '../dataset/11K_Hands/hands_masks/'
-# object_path = '../dataset/11K_Hands/hands_imgs/'
-# object_list_path = '../dataset/11K_Hands/hand_list.txt'
-
 object_list = []
 f = open(object_list_path, 'r')
 while True:
@@ -96,28 +78,22 @@ for dir in dirs:
 
                             mask_id = random.randint(0,mask_number-1)
                             mask_name = object_list[mask_id][:-1] + '.png'  # object
-                            # mask_name = object_list[mask_id][:-1]+'.jpg' # hand
                             mask = cv2.imread(os.path.join(mask_path, mask_name), cv2.IMREAD_GRAYSCALE)
                             mask = cv2.resize(mask, (256, 256))
 
                             num_pixel = len(mask[mask==255]) # number of pixel that has 255
                             cur_ratio = num_pixel / (256 * 256)
                             resize_scale = target_ratio / cur_ratio
-                            # I think we should root resize_scale
                             resize_scale = math.sqrt(resize_scale)
 
-                            # new_mask_name = os.path.join(new_ids_mask, mask_name)
                             new_mask_name = os.path.join(new_ids_mask, name)
 
                             object_name = object_list[mask_id][:-1]+'.jpeg' # object
-                            # object_name = object_list[mask_id][:-1] + '.jpg'  # hand
 
                             object = cv2.imread(os.path.join(object_path, object_name))
 
                             mask_y, mask_x  = mask.shape[0], mask.shape[1]
 
-                            #### mask: resize and reshape at random ####
-                            # resize_scale = random.uniform(0.05, 0.1)
                             desired_x = min(256, int(mask_x * resize_scale))
                             desired_y = min(256, int(mask_y * resize_scale))
                             desired_size = (desired_x, desired_y)
@@ -125,7 +101,6 @@ for dir in dirs:
                             new_pixel = len(mask[mask == 255])
                             object = cv2.resize(object, desired_size)
 
-                            # set a position at random (but within some meaningful range)
                             canvas = np.zeros((256,256))
                             if locat == 'center':
                                 init_x = 128 - (desired_size[0] // 2)
@@ -151,14 +126,9 @@ for dir in dirs:
 
                             mask_end_row = 256 - init_x if end_row >= 256 else desired_size[0]
                             mask_end_col = 256 - init_y if end_col >= 256 else desired_size[1]
-                            # print("init_x, end_row, init_y, end_col, mask_end_row, mask_end_col: ", init_x, end_row, init_y, end_col, mask_end_row, mask_end_col)
-                            canvas[init_x:end_row, init_y:end_col] = mask[:mask_end_row, :mask_end_col] # canvas: (256, 256)
+                            canvas[init_x:end_row, init_y:end_col] = mask[:mask_end_row, :mask_end_col]
                             new_canvas = np.zeros((256, 256, 3), dtype=np.uint8)
                             new_canvas[init_x:end_row, init_y:end_col,:] = object[:mask_end_row, :mask_end_col,:]
-
-                            # cv2.imshow('new canvas', new_canvas)
-                            # cv2.waitKey(0)
-                            # cv2.destroyAllWindows()
 
                             final_canvas = np.zeros((256, 256, 3))
                             final_mask = np.zeros((256, 256))
@@ -167,14 +137,9 @@ for dir in dirs:
                                 for j in range(256):
                                     if canvas[i,j] == 255: #if mask is white
                                         final_img[i,j,:] = new_canvas[i,j,:] # change image pixel into object pixel
-                                        # final_canvas[i, j, :] = new_canvas[i, j, :]
                                         final_mask[i, j] = 255
-                            # cv2.imshow('final_img', final_img)
-                            # cv2.waitKey(0)
-                            # cv2.destroyAllWindows()
 
                             cv2.imwrite(new_mask_name, final_mask)
-                            # cv2.imwrite(new_mask_name, canvas)
 
                             # save new img
                             real_final_path = os.path.join(new_ids, real_img_name)
